7376d75e412c69379d92e2f58366b82a92acc2e7
AbhiBUG <abhishekj2545@gmail.com>
2025-09-03 13:47:17 +0530

    Update login.jsx

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -28,7 +28,7 @@ const Login = ({setName}) => {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:5000/login", {
+      const res = await fetch("https://sqlmelabackend.onrender.com/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
