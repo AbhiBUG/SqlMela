53330c6d8a72462d09573f6f47adde95ed3b3374
AbhiBU <abhishekj2545@gmail.com>
2026-08-06 13:00:06 +0530

    Deployment 1)  express api changed

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -32,7 +32,7 @@ const Login = ({ setUser }) => {
 
     try {
       console.log(username,password);
-      const res = await fetch("http://localhost:5000/login", {
+      const res = await fetch("https://sqlmela.onrender.com/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
