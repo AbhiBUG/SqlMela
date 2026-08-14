7623148b4f53c7c7a92d3901972c7be13cc9587a
AbhiBU <abhishekj2545@gmail.com>
2026-08-14 08:59:21 +0530

    diagnosing middleware for session secure authorization

--- backend/middlewares.js
+++ backend/middlewares.js
@@ -1,7 +1,7 @@
 export const checkLoggedIn = (req,res,next)=>{
-    //  console.log("Session ID:", req.sessionID);
-    // console.log("Session:", req.session);
-    // console.log("User:", req.session.user);
+     console.log("Session ID:", req.sessionID);
+    console.log("Session:", req.session);
+    console.log("User:", req.session.user);
     if(req.session.user)
     {
         console.log("user generated");
